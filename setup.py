543243c22d00579e8d32c068cee147a472f67798
ca-mi-lo <camilo.ab.sc@gmail.com>
2023-07-28 19:22:52 -0600

    Nicer inputs

--- setup.py
+++ setup.py
@@ -11,6 +11,14 @@ setup(
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='hhttps://github.com/ca-mi-lo/pgbackup',
-    packages=find_packages('src')
+    url='https://github.com/ca-mi-lo/pgbackup',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
+
 )
